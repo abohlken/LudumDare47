03e00ab30928a62c08a27942b86da8d39e2c0b1c
Alex Bohlken <alexbohlken@gmail.com>
2020-10-02 22:53:14 -0500

    Started making stage move instead of car

--- Scenes/PlayerCar.gd
+++ Scenes/PlayerCar.gd
@@ -1,8 +1,52 @@
-extends Spatial
+extends KinematicBody
+
+signal hit_loop_beginning
+
+export var MAX_STEER_ANGLE = 0.5
+
+export var steer_speed = 5.0
+
+var steer_target = 0.0
+var steer_angle = 0.0
+
+export var joy_steering = JOY_ANALOG_LX
+export var steering_mult = -1.0
+
+export var position = Vector3()
+var velocity = Vector3(0,0,-50)
+var floorNormal = Vector3(0,1,0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    set_physics_process(true)
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
+func _physics_process(delta):
+	var steer_val = steering_mult * Input.get_joy_axis(0, joy_steering)
+	if Input.is_action_pressed("ui_left"):
+		steer_val = 1.0
+	elif Input.is_action_pressed("ui_right"):
+		steer_val = -1.0
+
+	steer_target = steer_val * MAX_STEER_ANGLE
+	if (steer_target < steer_angle):
+		steer_angle -= steer_speed * delta
+		if (steer_target > steer_angle):
+			steer_angle = steer_target
+	elif (steer_target > steer_angle):
+		steer_angle += steer_speed * delta
+		if (steer_target < steer_angle):
+			steer_angle = steer_target
+	#steering = steer_angle
+	
+	if(translation.z > 0):
+		moveCar()
+	else:
+		emit_signal("hit_loop_beginning")
+	
+func moveCar():
+	position = position + move_and_slide(velocity,floorNormal)
+	rotation.x += get_floor_normal().x
+	floorNormal.x = get_floor_normal().x
+	print(translation)
--- Scenes/Racetrack.gd
+++ Scenes/Racetrack.gd
@@ -1,16 +1,17 @@
 extends Spatial
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var rotateStage = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    pass
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#    pass
+func _process(delta):
+	if(rotateStage):
+		$"loop the loop".rotate_x(-delta)
+
+
+func _on_PlayerCar_hit_loop_beginning():
+	rotateStage = true
